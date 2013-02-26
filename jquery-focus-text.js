2b2ff2e3f55328508943ba2773f54a72c479524b
ninnypants <tyrel@ninnypants.com>
2013-02-26 10:48:30 -0800

    Initial commit

---
+++ jquery-focus-text.js
@@ -0,0 +1,23 @@
+(function($){
+	$.fn.focusText = function(){
+		return this.each(function(){
+			var $this = $(this);
+			console.log($this);
+			$this.data('focustext', $this.val());
+
+			$this.focus(function(){
+				var val = $this.data('focustext');
+				if(val == $this.val()){
+					$this.val('');
+				}
+			});
+
+			$this.blur(function(){
+				if($this.val() == ''){
+					$this.val($this.data('focustext'));
+				}
+			});
+
+		});
+	}
+})(jQuery);
